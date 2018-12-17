02dd75b3969f3c1ca387dbf459e1372cebfa7436
murikarox <eksi@kth.se>
2018-12-17 20:00:51 +0100

    additional setup

---
+++ django-entrypoint.sh
@@ -0,0 +1,14 @@
+until cd src
+do
+    echo "Waiting for django volume..."
+    sleep 2
+done
+
+until python manage.py migrate
+do
+    echo "Waiting for postgres ready..."
+    sleep 2
+done
+
+
+python manage.py runserver 0.0.0.0:8000
